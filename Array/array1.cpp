af54d6f0aa2033f9a1b933080ed3ca2665bfb03b
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-01 21:03:36 +0530

    Improved various fucntions of array

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -3,7 +3,6 @@
 // index -> location of an elemnent
 // indexing starts from 0
 
-
 #include<iostream>
 using namespace std;
 
@@ -17,6 +16,19 @@ void input(int arr[],int n){
 void append(int arr[],int &n){
     cout<<"Enter last 1 element: ";
     cin>>arr[n];
+    n++;
+}
+
+void insert(int arr[],int &n){
+    int x,p;
+    cout<<"Enter index where you want to insert the element: ";
+    cin>>p;
+    cout<<"Enter the element: ";
+    cin>>x;
+    for(int i=n;i>p;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[p]=x;
     n++;
 }
 
@@ -36,9 +48,9 @@ int main(){
 
     input(arr,n);
 
-    append(arr,n);
+    insert(arr,n);
 
     output(arr,n);
-    
+
     return 0;
 }
