5b71412d82652bd8f204f21930d3ec80840a9861
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-12 14:46:02 +0530

    Improvised the typos and errors making it difficult to run the program

--- 2D array/3D_array.cpp
+++ 2D array/3D_array.cpp
@@ -14,15 +14,15 @@ int main(){
     int arr[l][c][r];
     for(int i=0;i<l;i++){
         for(int j=0;j<c;j++){
-            for(int k=0;i<r;k++){
+            for(int k=0;k<r;k++){
                 cin>>arr[i][j][k];
             }
         }
     }
-    cout<<"Your Enterd Array is: ";
+    cout<<"Your Enterd Array is: "<<endl;
     for(int i=0;i<l;i++){
         for(int j=0;j<c;j++){
-            for(int k=0;i<r;k++){
+            for(int k=0;k<r;k++){
                 cout<<arr[i][j][k]<<" ";
             }
             cout<<endl;
