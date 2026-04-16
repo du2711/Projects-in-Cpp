0ed8e75d6e154e38564f54b03123b311c0aa45c9
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-16 16:23:08 +0530

    Added a pascal triangle printing program upto n

---
+++ 2D vectors/pascalTrinagle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fact(int n){
+    if(n==0 || n==1) return 1;
+    else return n*(fact(n-1));
+}
+
+int comb(int n,int r){
+    int ncr = fact(n)/(fact(r)*fact(n-r));
+    return ncr;
+}
+
+int main(){ 
+    int n;
+    cout<<"Enter upto digit: ";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            int pas=comb(i,j);
+            cout<<pas<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
