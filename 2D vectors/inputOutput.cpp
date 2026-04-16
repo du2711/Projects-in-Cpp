42b3a44d35ca1c6634fc5835e12b8fa9373f010c
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-16 16:03:45 +0530

    Added Matrix Problems using vectors

---
+++ 2D vectors/inputOutput.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){ 
+    int r,c;
+    cout<<"Enter size of row: ";
+    cin>>r;
+    cout<<"Enter size of column: ";
+    cin>>c;
+
+    vector<vector<int>> arr(r,vector<int>(c));
+
+    cout<<"Enter Your Matrix: "<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"Enterd Matrix is: "<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
