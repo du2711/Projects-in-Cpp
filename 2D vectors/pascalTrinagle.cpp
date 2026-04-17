4890c342e54ad79dfd81c73d50d15d0a5e7f4ff7
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-17 11:38:31 +0530

    optimised data oevrflow control using vectors

--- 2D vectors/pascalTrinagle.cpp
+++ 2D vectors/pascalTrinagle.cpp
@@ -12,21 +12,49 @@ int comb(int n,int r){
     return ncr;
 }
 
+vector<vector<int>> pascalTriangle(int n){
+    vector<vector<int>> pascal(n);
+
+    for(int i=0;i<n;i++){
+        pascal[i].resize(i+1);
+
+        for(int j=0;j<i+1;j++){
+            if(j==0 || j==i) pascal[i][j]=1;
+            else pascal[i][j]=pascal[i-1][j] + pascal[i-1][j-1];
+        }
+    }
+    return pascal;
+}
+
 int main(){ 
     int n;
     cout<<"Enter upto digit: ";
     cin>>n;
 
-    for(int i=0;i<n;i++){
+    vector<vector<int>> ans;
+
+    ans = pascalTriangle(n);
+
+    for(int i=0;i<ans.size();i++){
         for(int k=0;k<n-i;k++){
             cout<<" ";
         }
-        for(int j=0;j<=i;j++){
-            int pas=comb(i,j);
-            cout<<pas<<" ";
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
         }
         cout<<endl;
     }
+
+    // for(int i=0;i<n;i++){
+    //     for(int k=0;k<n-i;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<=i;j++){
+    //         int pas=comb(i,j);
+    //         cout<<pas<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
     return 0;
 }
