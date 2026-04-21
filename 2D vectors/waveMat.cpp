8d10f7933f4fbc0d2e2fa9a276e3850d3c1722c0
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-21 21:30:26 +0530

    Made the wave matrix correct for rows

--- 2D vectors/waveMat.cpp
+++ 2D vectors/waveMat.cpp
@@ -5,14 +5,17 @@ using namespace std;
 void wave(vector<vector<int>> &v){
     for(int i=0;i<v.size();i++){
         if(i%2==0){
-            for(int j=0;j<v[0].size();j++){
+            for(int j=0;j<v[i].size();j++){
                 cout<<v[i][j]<<" ";
             }
         }
         else{
-            
+            for(int j=v[i].size()-1;j>=0;j--){
+                cout<<v[i][j]<<" ";
+            }
         }
     }
+    cout<<endl;
 }
 int main(){ 
     int r,c;
@@ -30,12 +33,10 @@ int main(){
         }
     }
 
-    cout<<"Entered Matrix is: "<<endl;
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    cout<<"Waved Matrix is: "<<endl;
+    wave(arr);
+
+    
+    
     return 0;
 }
