a7522b9951a2a7b65f8c91ab3a41cdb91f454dc1
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-21 21:37:06 +0530

    optimised tha matrix for both column and rows

--- 2D vectors/waveMat.cpp
+++ 2D vectors/waveMat.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void wave(vector<vector<int>> &v){
+void waveRow(vector<vector<int>> &v){
     for(int i=0;i<v.size();i++){
         if(i%2==0){
             for(int j=0;j<v[i].size();j++){
@@ -11,6 +11,22 @@ void wave(vector<vector<int>> &v){
         }
         else{
             for(int j=v[i].size()-1;j>=0;j--){
+                cout<<v[i][j]<<" ";
+            }
+        }
+    }
+    cout<<endl;
+}
+
+void waveCol(vector<vector<int>> &v){
+    for(int j=0;j<v.size();j++){
+        if(j%2==0){
+            for(int i=0;i<v[j].size();i++){
+                cout<<v[i][j]<<" ";
+            }
+        }
+        else{
+            for(int i=v[j].size()-1;i>=0;i--){
                 cout<<v[i][j]<<" ";
             }
         }
@@ -33,9 +49,14 @@ int main(){
         }
     }
 
-    cout<<"Waved Matrix is: "<<endl;
-    wave(arr);
+    cout<<"Enter Choice: "<<endl<<"1.Row"<<endl<<"2.Column"<<endl;
+    int t;
+    cin>>t;
 
+    cout<<"Waved Matrix is: "<<endl;
+    if(t==1) waveRow(arr);
+    else if(t==2) waveCol(arr);
+    else cout<<"Enter Valid Input";
     
     
     return 0;
