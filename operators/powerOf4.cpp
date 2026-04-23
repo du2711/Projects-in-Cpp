2ce5e6f94948b2095c0f164739979d75374d32d1
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-23 11:29:53 +0530

    Improved the bugs

--- operators/powerOf4.cpp
+++ operators/powerOf4.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 bool powerOf4(int n){
-    if (n%4==0 && ((n-1)%3==0)) return true;
-    else return false;
+    return n>0 && (n&(n-1))==0 && ((n-1)%3==0);
 }
 
 int main(){ 
