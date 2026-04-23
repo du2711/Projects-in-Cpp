ac89719f00df09975fc512d9666e44a829a7232a
du2711 <deepanshuupadhyay603@gmail.com>
2026-04-23 18:37:05 +0530

    still working on matrix sum part

---
+++ 2D vectors/matrixPartSum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){ 
+    int r,c;
+    cout<<"Enter size of row: ";
+    cin>>r;
+    cout<<"Enter size of column: ";
+    cin>>c;
+
+    vector<vector<int>> arr(r,vector<int>(c));
+
+    cout<<"Enter Your Matrix: "<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"Entered Matrix is: "<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int left,right,top,bottom;
+
+    cout<<"Enter starting row index: ";
+    cin>>left;
+
+    cout<<"Enter ending column index: ";
+    cin>>right;
+
+    cout<<"Enter ending row index: ";
+    cin>>bottom;
+
+    
+    
+    return 0;
+}
