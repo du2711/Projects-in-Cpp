9ece6f554c8e7c65df717e3e892c9b6abefe1027
du2711 <deepanshuupadhyay603@gmail.com>
2026-05-08 16:02:48 +0530

    before linked list, oops

---
+++ linked List/oops1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class fruit{
+    public:
+    string name;
+    string color;
+};
+
+int main(){ 
+    
+    fruit apple;
+    apple.name="Apple";
+    apple.color="Red";
+
+    return 0;
+}
