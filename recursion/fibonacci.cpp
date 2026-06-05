cef252a9a4ac280df902798235dd95c792fb87f7
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-05 14:21:47 +0530

    fibonacci using recursion

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fibo(int n){
+    if(n<=1) return n;
+    else return fibo(n-1)+fibo(n-2);
+}
+
+int main(){ 
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<fibo(n);
+    return 0;
+}
