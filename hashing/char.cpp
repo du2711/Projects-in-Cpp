e9a91380f499033d3341133804f54097d05dbad2
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-07 21:40:23 +0530

    learning character hashing

--- hashing/char.cpp
+++ hashing/char.cpp
@@ -2,7 +2,32 @@
 #include<vector>
 using namespace std;
 
+int hashh[256];
 int main(){ 
-    
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    char arr[n];
+    cout<<"Enter your elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        arr[i]=arr[i]-'a';
+    }
+
+    for(int i=0;i<n;i++){
+        hashh[i]=char(arr[i]+1);
+    }
+
+    int q;
+    cout<<"Enter no. of queries: ";
+    cin>>q;
+
+    while(q--){
+        int num;
+        cout<<"Enter query: ";
+        cin>>num;
+        cout<<hashh[num];
+    }
     return 0;
 }
