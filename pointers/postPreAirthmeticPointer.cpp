31c29b585ebcfa260102e14b9c2af04b2d105160
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-10 22:05:25 +0530

    very minute and basic but very strong and conceptualised way to understand

--- pointers/postPreAirthmeticPointer.cpp
+++ pointers/postPreAirthmeticPointer.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){ 
     
     int arr[4]={1,2,3,4};
 
-    int *ptr=&arr[0];
+    // int *ptr=&arr[0];
 
-    cout<<ptr<<" "<<*ptr<<endl;
-    cout<<ptr+1<<" "<<*(ptr+1)<<endl;
+    // cout<<ptr<<" "<<*ptr<<endl;
+    // cout<<ptr+1<<" "<<*(ptr+1)<<endl;
 
-    *ptr++;
-    cout<<*ptr<<endl;
+    // *ptr++;
+    // cout<<*ptr<<endl;
 
-    cout<<(*ptr++)<<endl;
+    // cout<<(*ptr++)<<endl;
+
+
+    // *ptr++
+    // (*ptr)++
+    // *++ptr
+    // ++*ptr
+    
+    int brr[4]={6,7,8,91};
+    int* p = &brr[0];
+
+    cout<<*p++<<endl;       // arr[0]->6
+    cout<<(*p)++<<endl;     // arr[1]->7
+    cout<<*++p<<endl;       // arr[2]->8
+    cout<<++*p<<endl;       // arr[2]+1-> 9
+
     return 0;
 }
