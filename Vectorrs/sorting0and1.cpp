aa6b1b0561fdfb24ba8badadce2ef37a3d6f7858
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-13 15:25:14 +0530

    sort array for 1 and 0

---
+++ Vectorrs/sorting0and1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void swap(int& a,int& b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+void sort01(vector<int>& arr){
+    int i=0,j=arr.size()-1;
+    while(i<j){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j--;
+        }
+        else if(arr[i]==0) i++;
+    }
+}
+int main(){ 
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter your array: ";
+    for(int i=0;i<n;i++){
+        int ele;
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    cout<<"Enterd array is: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    sort01(arr);
+    cout<<endl<<"sorted array is: ";
+    for(int ele: arr){
+        cout<<ele<<" ";
+    }
+    return 0;
+}
