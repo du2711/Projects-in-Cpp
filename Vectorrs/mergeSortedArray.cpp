9f3f25b90bd4fd789cdf982472606668c3b77307
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-15 00:02:59 +0530

    tried more optimisation

--- Vectorrs/mergeSortedArray.cpp
+++ Vectorrs/mergeSortedArray.cpp
@@ -2,6 +2,15 @@
 #include<vector>
 using namespace std;
 
+void swap(int& a,int& b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+void swapGreat(vector<int> &arr,vector<int> &brr,int idx1,int idx2){
+    if(arr[idx1]>brr[idx2]) swap(arr[idx1],brr[idx2]);
+}
+
 void mergeArray(vector<int> &arr,vector<int> &brr){
     int n=arr.size();
     int m=brr.size();
@@ -35,14 +44,39 @@ void mergeArray(vector<int> &arr,vector<int> &brr){
     //     else brr[i-n]=resArr[i];
     // }
 
-    int left=n-1;
-    int right=m-1;
-    while(left>=0 && right<m){
-        if(arr[left]>brr[right]){
-            swap(arr[left],brr[right]);
-            left--,right++;
+    // int left=n-1;
+    // int right=m-1;
+    // while(left>=0 && right<m){
+    //     if(arr[left]>brr[right]){
+    //         swap(arr[left],brr[right]);
+    //         left--,right++;
+    //     }
+    //     else break;
+    // }
+
+
+    int len= (n+m);
+    int gap=(len/2)+(len%2);
+    while(gap>0){
+        int left=0;
+        int right=left+gap;
+        while(right<len){
+            //arr and brr
+            if(left<n&&right>=n){
+                swapGreat(arr,brr,left,right-n);
+            }
+            // brr and brr 
+            else if(left>=n){
+                swapGreat(brr,brr,left-n,right-n);
+            }
+            // arr and arr
+            else{
+                swapGreat(arr,arr,left,right);
+            }
+            left++,right++;
         }
-        else break;
+        if(gap==1) break;
+        gap=(gap/2)+(gap%2);
     }
 }
 
@@ -71,6 +105,6 @@ int main(){
         brr.push_back(ele);
     }
 
-    mergeArray(arr,brr);
+    cout<<mergeArray(arr,brr);
     return 0;
 }
