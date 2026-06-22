3c925b8f2656a0c6da81736375c8874c6f5ff661
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-22 13:15:03 +0530

    more optimization using fast exponentiation

--- recursion/powerExpo.cpp
+++ recursion/powerExpo.cpp
@@ -2,9 +2,27 @@
 #include<vector>
 using namespace std;
 
+int powerOp(int b,int p){
+    if(p==0) return 1;
+    if(p%2 ==  0){
+        int res=powerOp(b,p/2);
+        return res*res;
+    }
+    else{
+        int res = powerOp(b,(p-1)/2);
+        return b*res*res;
+    }
+}
+
 int power(int b,int p){
     if(p==0 || p==1) return b;
     else return b*power(b,p-1);
+
+    // if(p==0 || p==1) return b;
+    // else {
+    //     if(p%2==0) return power(power(b,p/2),2);
+    //     else return power(power(b,(p-1)/2),2);
+    // }
 }
 
 int main(){ 
@@ -14,6 +32,6 @@ int main(){
     cout<<"Enter poewr: ";
     cin>>p;
 
-    cout<<power(b,p);
+    cout<<powerOp(b,p);
     return 0;
 }
