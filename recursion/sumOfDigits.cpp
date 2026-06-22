dd3951d9240b137c67c45445ae4f8ab594dde2d5
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-22 11:54:18 +0530

    some practice on recursions

--- recursion/sumOfDigits.cpp
+++ recursion/sumOfDigits.cpp
@@ -2,15 +2,16 @@
 #include<vector>
 using namespace std;
 
-int fibo(int n){
-    if(n<=1) return n;
-    else return fibo(n-1)+fibo(n-2);
+int sumDigit(int n){
+    if(n>0 && n<10) return n;
+    else 
 }
 
 int main(){ 
     int n;
     cout<<"Enter n: ";
     cin>>n;
-    cout<<fibo(n);
+
+    cout<<sumDigit(n);
     return 0;
 }
