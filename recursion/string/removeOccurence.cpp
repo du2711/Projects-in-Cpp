b2dda98e0063a4788f8dd7ea1f6c055475108f2a
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-24 12:16:41 +0530

    modified many things forgot to commit

--- recursion/string/removeOccurence.cpp
+++ recursion/string/removeOccurence.cpp
@@ -2,7 +2,34 @@
 #include<vector>
 using namespace std;
 
+string occurance(string &str,int idx,char target){
+
+    //time -> O(n^2) in worst case 
+    //space -> O(n)
+    if(idx == str.size()) return str;
+    if(str[idx]==target){
+        str.erase(idx,1);
+        return occurance(str,idx,target); 
+    }
+    else return occurance(str,idx+1,target);
+
+    // time O(n^2)
+    // space -> O(n^2)
+    if(idx == str.size()) return "";
+    string curr = "";
+    curr= curr+str[idx];
+    return ((str[idx] == target) ? "" : curr) + occurance(str,idx+1,target);
+}
+
 int main(){ 
-    
+    string str;
+    cout<<"Enter string: ";
+    getline(cin,str);
+
+    char tar;
+    cout<<"Enter target deletion: ";
+    cin>>tar;
+
+    cout<<"Edited String: "<<occurance(str,0,tar);
     return 0;
 }
