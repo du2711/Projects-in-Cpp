9fd314c5df2a4742ea665bc5ec436865d54a4e22
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-25 12:28:24 +0530

    sequential n natural numbers

--- recursion/increasingSequence.cpp
+++ recursion/increasingSequence.cpp
@@ -2,9 +2,22 @@
 #include<vector>
 using namespace std;
 
+void n2one(int n){
+    if(n==0) return;
+    cout<<n<<" ";
+    n2one(n-1);
+}
+
+void one2n(int n){
+    if(n==0) return;
+    one2n(n-1);
+    cout<<n<<" ";
+}
 
 
 int main(){ 
-    
+    one2n(9);
+    cout<<endl;
+    n2one(9);
     return 0;
 }
