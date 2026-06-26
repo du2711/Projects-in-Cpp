26aa50d9135693c862abb26846ab92c5fc5a6c98
du2711 <deepanshuupadhyay603@gmail.com>
2026-06-26 15:09:39 +0530

    armstrong number check using iteration and recursive optimised power check with O(log n)

---
+++ recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+int power(int a,int b){
+    if(b==0) return 1;
+    int half=power(a,b/2);
+
+    if(b%2==0) return half*half;
+    else return a*half*half;
+}
+
+bool armstrong(int a){
+    int arm=a;
+    int digit=0;
+
+    int temp=a;
+    while(temp!=0){
+        digit++;
+        temp/=10;
+    }
+
+    int sum=0;
+    while(a!=0){
+        int remain;
+        remain=a%10;
+        sum=sum+power(remain,digit);
+        a/=10;
+    }
+    
+    if(arm==sum) return true;
+    else return false;
+}
